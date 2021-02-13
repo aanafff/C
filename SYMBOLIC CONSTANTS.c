eef4c93075c3025dcf56aade97d9b7c314b81753
aanafff <78566455+aanafff@users.noreply.github.com>
2021-02-12 16:39:25 -0800

    Add files via upload

---
+++ SYMBOLIC CONSTANTS.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  SYMBOLIC CONSTANTS
+//
+//  Created by Ana Ramirez on 12/02/21.
+//
+#include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+main()
+{
+    int fahr;
+    for(fahr=LOWER;fahr<=UPPER;fahr=fahr+STEP)
+    printf("%3d%6.1f/n",fahr,(5.0/9.0)*(fahr-32));
+}
