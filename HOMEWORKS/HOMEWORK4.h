06718b7de658aee2b38d49e6f0fe13c64da301c4
aanafff <78566455+aanafff@users.noreply.github.com>
2021-04-14 20:15:56 -0700

    Add files via upload

---
+++ HOMEWORKS/HOMEWORK4.h
@@ -0,0 +1,14 @@
+//
+//  HOMEWORK4.h
+//  HOMEWORK4
+//
+//  Created by Ana Ramirez on 14/04/21.
+//
+
+#define MAX 100
+
+void push(double);
+double pop(void);
+int getop(char[]);
+int getch(void);
+void ungetch(int);
