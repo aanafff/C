70bc91e88aa5892c68b5d4a2419cd6f7e8bf8ebe
aanafff <78566455+aanafff@users.noreply.github.com>
2021-05-06 12:11:35 -0700

    Add files via upload

---
+++ HOMEWORKS/HOMEWORK7.h
@@ -0,0 +1,18 @@
+//
+//  HOMEWORK7.h
+//  HOMEWORK7
+//
+//  Created by Ana Ramirez on 06/05/21.
+//
+
+#define HOMEWORK7_h
+#define MAX 30
+#define USUARIOS    50 
+int* registrados = 0;
+int** matrix;
+int totaldesalidas=0;
+int count();
+void registrar();
+void entradaosalida(int, int);
+void header();
+void horario();
